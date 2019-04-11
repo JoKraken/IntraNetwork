59f2524210fa8a342c5c25ca4e9ff0f763c0748b
jaya <bidarilife36@gmail.com>
2019-04-11 16:15:30 +0300

    generic error handler added

--- server/server.js
+++ server/server.js
@@ -2,14 +2,15 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const errorHandler = require("./handlers/error");
 
-const PORT = 8081;
+const PORT = 3001;
 
 app.use(cors());
 app.use(bodyParser.json());
 
 app.get("/", (req, res, next) => {
-  res.send("WELCOME Jaya");
+  res.send("jaya welcome");
 });
 
 app.use(function(req, res, next) {
@@ -17,6 +18,7 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
+app.use(errorHandler);
 
 app.listen(PORT, function() {
   console.log(`Server is running on Port ${PORT}`);
