7a1d2d1a4ec35a43e2025763beb524b07634e985
jaya <bidarilife36@gmail.com>
2019-04-18 23:12:40 +0300

    redux store setup, navbar component added

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,27 +1,19 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import { Provider } from "react-redux";
+import { configureStore } from "../store";
+import { BrowserRouter as Router } from "react-router-dom";
+import Navbar from "./Navbar";
+const store = configureStore();
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
-
+const App = () => {
+  return (
+    <Provider store={store}>
+      <Router>
+        <div className="onboarding">
+          <Navbar />
+        </div>
+      </Router>
+    </Provider>
+  );
+};
 export default App;
